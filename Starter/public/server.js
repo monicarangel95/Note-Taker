481a0c404a1b41c430b4dca79a566c45c85ed4d2
Monica Rangel <monicarangel95@github.com>
2020-07-26 11:58:54 -0700

    Setting up router and listener
    // ROUTER
    // The below points our server to a series of "route" files.
    // These routes give our server a "map" of how to respond when users visit or request data from various URLs.// LISTENER
    // The below code effectively "starts" our server

--- Starter/public/server.js
+++ Starter/public/server.js
@@ -22,3 +22,13 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ======================
+
+// require("./routes/routes")(app);
+
+// ======================
+// LISTENER
+// The below code effectively "starts" our server
+// ======================
+app.listen(PORT,function(){
+    console.log("App is listening on PORT: " + PORT);
+});
