9d1058997e3cf1fc9a3a061f58e24944a2773ce2
Monica Rangel <monicarangel95@github.com>
2020-07-27 20:11:04 -0700

    server updates

--- server.js
+++ server.js
@@ -2,84 +2,102 @@
 // DEPENDENCIES
 //=====================
 //Series of npm packages that will give our server functionality//
-var express = require("express");
+const express = require("express");
 const fs = require("fs");
-var path = require("path");
+const path = require("path");
+
 //=====================
 // EXPRESS CONFIGURATION
 //=====================
 // This sets up the basic properties for our express server
 // Tells node that we are creating an "express" server
-var app = express();
-
+const app = express();
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //Serve a public folder
 app.use(express.static("public"));
 
-//Load the note list from file
-let noteList = parseNotes()
+let notes = [];
+let id;
 
-//A function to read/write db.JSON note file
-function parseNotes(){
-    const noteList = JSON.parse( fs.readFileSync( './db/db.json',  'utf8') )
-    return noteList
-}
-function saveNotes() {
-    fs.writeFileSync( '/db/db.json', JSON.stringify(noteList))
-}
+//Storing read/write Note variables to make it easier to reference later on
+const readNotes = () => {
+    fs.readFile(__dirname + "/db/db.json", (err, response) => {
+        if (err) throw err;
+        notes = JSON.parse(response); //Parse to ensure we are dealing with array of objects
+    });
+};
+
+const writeNotes = () => {
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+        if (err) throw err; //Using JSON.stringify to ensure notes rendered are strings
+    });
+};
 
 // ======================
 // HTML ROUTER
 // ======================
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// If no matching route is found default to home
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+
 // ========================
 // API ROUTES
 //=========================
-// Get All notes
-app.get('/api/notes', function(req, res) {
-    // console.log('get notes')
-    res.send( noteList )
-})
-
 // ADD NOTE
-app.post('/api/notes', function(req, res) {
-    let newNote = req.body
-    // Creates a new note ID
-    newNote.id = Date.now()
-    noteList.push(newNote)
-    saveNotes()
-    res.send( newNote )
-})
+app.get("/api/notes", (req, res) => {
+    readNotes();
+    // Read the db.json file and return all saved notes as JSON.
+    res.json(notes);
+});
+
+// Setup the /api/notes POST route
+app.post("/api/notes", (req, res) => {
+
+    //Allows for unique ID's
+    newNote = req.body;
+    id = notes.length + 1;
+    newNote.id = id++;
+
+    notes.push(newNote);
+
+    writeNotes();
+    console.log("Note written to db.json");
+
+    res.json(notes);
+
+});
+
 
 // DELETE NOTE
-app.delete( '/api/notes/:id', function( req, res ){
-    const num = req.params.id
-    noteList = noteList.filter( note=>note.id != num )
-    saveNotes()
-    res.send( { id: num, message: 'Note Removed', status: true } )
-})
 
-// ======================
-// LISTENER
-// The below code effectively "starts" our server
-// ======================
+    // Stores the Id parameter from the API
+    app.delete("/api/notes/:id", (req, res) => {
+        var chosenId = parseInt(req.params.id);
+        let foundNote = notes.find(note => note.id === chosenId);
 
-app.listen(PORT, function () {
-    console.log("App is listening on PORT: " + PORT);
-});
+        notes.splice(notes.indexOf(foundNote), 1);
+
+        writeNotes();
+        console.log("Note deleted from db.json");
+
+        readNotes();
+
+        res.json(foundNote);
+    })
+
+    // ======================
+    // LISTENER
+    // The below code effectively "starts" our server
+    // ======================
+    app.listen(PORT, function () {
+        console.log("App is listening on PORT: " + PORT);
+    });
